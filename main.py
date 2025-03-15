d319847d6b1e1341b7c226b6fcaef59eb0f3ac61
Ferihidayat94 <ferihidayat94@gmail.com>
2025-03-16 04:59:36 +0700

    main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ hide_streamlit_style = """
     footer {visibility: hidden;}
     header {visibility: hidden;}
     .st-emotion-cache-16txtl3 {display: none;} /* Menghilangkan tombol Fork */
+    .stButton>button {background-color: green !important; color: white !important;} /* Mengubah warna tombol */
     </style>
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
